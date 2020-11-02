82134dbe1e9b4fffd07b96e3aa71b9f34a6658d4
sermor95 <sermor95@gmail.com>
2020-11-02 12:50:18 +0100

    Docker compose

--- app.py
+++ app.py
@@ -4,9 +4,10 @@ from myprocessor import FeatureSelection
 from functions import *
 from repomongo import *
 import traceback
-import sys
+import redis
 
 app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
 
 @app.route('/', methods=["GET"])
 def homepage():
